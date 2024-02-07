7ccc7fa5120fba9c28718c6b170304c0a28d373a
AlvaroRK <adriano.rk06@gmail.com>
2024-02-07 02:58:00 -0300

    creacion del proyecto clon

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import A from './A'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <A />
+  </React.StrictMode>,
+)
